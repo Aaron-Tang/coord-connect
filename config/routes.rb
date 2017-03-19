0034224146978d2fc00f91319bd51af957d1e919
eddiequan <therealeddiequan@gmail.com>
2017-03-19 19:49:19 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   	resources :course
   	resources :applied_applicants
-	get '/courses', to: 'course#index'  
+	get '/courses', to: 'course#index'
+	get '/courses/:id', to: 'course#show'
+	post '/courses', to: 'course#create'
+	put '/courses/:id', to: 'course#update'
+
 	get '/courses/:id/applicants', to: 'course#all_applicants'
 end
