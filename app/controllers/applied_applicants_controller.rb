422e1c3f5802b91a2e53a83648b6222b53d153ca
eddiequan <therealeddiequan@gmail.com>
2017-03-20 00:00:35 -0400

    Update applied_applicants_controller.rb

--- app/controllers/applied_applicants_controller.rb
+++ app/controllers/applied_applicants_controller.rb
@@ -1,5 +1,4 @@
 class AppliedApplicantsController < ApplicationController
-skip_before_action :verify_authenticity_token
 before_action :set_application, only: [:show, :update, :destroy]
 
 	# GET /applications
