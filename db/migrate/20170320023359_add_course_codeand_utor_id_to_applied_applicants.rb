f666e32fab001b2201c8bb64b528625fa01a186e
Eddie Quan <therealeddiequan@gmail.com>
2017-03-22 16:15:39 -0400

    fixed broken migration

--- db/migrate/20170320023359_add_course_codeand_utor_id_to_applied_applicants.rb
+++ db/migrate/20170320023359_add_course_codeand_utor_id_to_applied_applicants.rb
@@ -1,6 +1,5 @@
 class AddCourseCodeandUtorIdToAppliedApplicants < ActiveRecord::Migration[5.0]
   def change
     add_column :applied_applicants, :utorid, :string
-    add_column :applied_applicants, :course_code, :string
   end
 end
